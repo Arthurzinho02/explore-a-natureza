df85943e971823682840b304746c767c7ea19172
Arthurzinho02 <arthur.matheus0607@gmail.com>
2023-10-16 19:13:18 -0300

    update

--- js/modules/form.js
+++ js/modules/form.js
@@ -1,20 +1,40 @@
 export default function form(){
-    const form = document.querySelector('.modal-forms')
+    const form = document.querySelector('.js-modal')
     const dados = {}
+   
 
-    function pegarvaiform(event){
+    if (localStorage.getItem("email") === null) {
+        form.classList.add("active");
+        }
+        else {
+        form.classList.remove("active");
+        }
+
+    function pegarValorForm(event){
         dados[event.target.name] = event.target.value
         console.log(dados)
     }
+    
+    // adiciona um listener para o envio do formulário
+    form.addEventListener('change', pegarValorForm)
+    document.querySelector(".modal-forms").addEventListener("submit", (event) => {
+    event.preventDefault();
+    localStorage.setItem("email", JSON.stringify(dados.email))
+    alert("Dados salvos com sucesso")
+    form.classList.remove("active");
+    });
 
-    form.addEventListener('change', pegarvaiform)
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        localStorage.setItem(dados.email, JSON.stringify(dados))
-        alert("Dados salvo com sucesso!")
-    })
+    // adiciona um listener para o botão de fechar
+    document.querySelector(".modal-close").addEventListener("click", () => {
+    // esconde o elemento
+    form.classList.remove("active");
+    });
 
-
+    if (localStorage.getItem("email") === null) {
+    setTimeout(() => {
+        form.classList.add("active");
+    }, 3000);
+    }
 
 
 }
